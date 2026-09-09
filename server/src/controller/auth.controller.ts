11ced6a159dbc14a4d7b09c40c7d60b5b294f703
Ayush Rana <ayushrana3091@gmail.com>
2026-09-09 16:29:16 +0530

    auth production

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -17,7 +17,7 @@ function setAuthCookie(res: Response, token: string){
   res.cookie("token",token,{
     httpOnly:true,
     secure: process.env.NODE_ENV==="production",
-    sameSite:"lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge:7*24*60*60*1000,// 7 days, in ms
   });
 }
