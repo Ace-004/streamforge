ef7fac88e19fa01bb4cec16cfd1e65b3e8d6f36b
Ayush Rana <ayushrana3091@gmail.com>
2026-09-13 13:31:43 +0530

    reconcile schedule job interval 2 day

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const httpServer = app.listen(PORT, async () => {
   console.log(`server is running on port ${PORT}`);
   await reconcileQueue.upsertJobScheduler(
     "reconcile-pending-videos-schedule",
-    { every: 24 * 60 * 60 * 1000 },
+    { every: 48 * 60 * 60 * 1000 },
     { name: "reconcile" },
   );
   startReconcileWorker();
